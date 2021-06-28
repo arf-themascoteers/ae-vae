7a0e7c525448249e6d4ff7af8063db22605ccc09
arif <OrangeBlu20R>
2021-06-28 23:56:54 +1000

    Done

--- sampling.py
+++ sampling.py
@@ -2,7 +2,6 @@ import torch
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from vae import VAELinear
-from decoder import Decoder
 
 vae = VAELinear()
 vae.load_state_dict(torch.load("models/vae.h5"))
@@ -18,21 +17,34 @@ def untargeted_generation():
             plt.show()
 
 def targeted_generation():
-    model = vae.VAELinear()
-    model.load_state_dict(torch.load("models/vae.h5"))
-    model.eval()
-
     mnist_data = datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
     data_loader = torch.utils.data.DataLoader(dataset=mnist_data, batch_size=10, shuffle=True)
+    fig = plt.figure()
+    SAMPLE = 10
+    count = 0
     with torch.no_grad():
-        for (img_original, _) in data_loader:
+        for (img_originals, _) in data_loader:
+            img_original = img_originals[0]
             img = img_original.reshape(-1, 28 * 28)
-            decoded_image, mean, log_var = model(img)
+            recon, _, _ = vae.encoder(img)
 
+            NR = 0.2
             noise = torch.randn(1,2)
-            generated_image = vae.decoder(noise)
-            generated_image = generated_image[0].reshape(28, 28)
-            plt.imshow(generated_image)
-            plt.show()
+            decoded_image = recon*(1-NR) + noise*NR
+            generated_image = vae.decoder(decoded_image)
 
-untargeted_generation()
+            recon_original = generated_image.reshape(28, 28)
+            original = img_original[0].detach().numpy()
+            made = recon_original.detach().numpy()
+            count = count + 1
+            fig.add_subplot(SAMPLE, 2, count)
+            plt.imshow(original)
+            count = count + 1
+            fig.add_subplot(SAMPLE, 2, count)
+            plt.imshow(made)
+            if count >= SAMPLE * 2:
+                break
+        plt.show()
+
+
+targeted_generation()
