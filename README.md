6fb717116086749feea6fe591425ddc03abb0f13
arif <OrangeBlu20R>
2021-06-28 23:29:33 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+train.py: To train the machine
+
+test.py: To test mimic
+
+sampling.py: To generate digit
---
+++ vae.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+import torch
+from decoder import Decoder
+from encoder import Encoder
+
+
+class VAELinear(nn.Module):
+    def __init__(self):
+        super(VAELinear, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        mean, log_var = self.encoder(x)
+        z = self.reparameterization(mean, log_var)
+        decoded = self.decoder(z)
+        return decoded, mean, log_var
+
+    def reparameterization(self, mean, var):
+        epsilon = torch.randn_like(var)
+        z = mean + var*epsilon
+        return z
