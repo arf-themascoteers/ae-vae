630c29a578ca6bffdce96ac47a3a47972f8bf804
arif <OrangeBlu20R>
2021-06-29 00:11:13 +1000

    Done

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import vae
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 import torchvision
+import time
 
 def calculate_loss(x, decoded, mean, log_var):
     reproduction_loss = F.binary_cross_entropy(decoded, x, reduction='sum')
@@ -11,39 +12,47 @@ def calculate_loss(x, decoded, mean, log_var):
     return reproduction_loss + KLD
 
 
-transform = transforms.ToTensor()
-mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-data_loader = torch.utils.data.DataLoader(dataset=mnist_data,
-                                          batch_size=64,
-                                          shuffle=True)
+def train():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    transform = transforms.ToTensor()
+    mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    data_loader = torch.utils.data.DataLoader(dataset=mnist_data,
+                                              batch_size=64,
+                                              shuffle=True)
 
-model = vae.VAELinear()
-model.train()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
-writer = SummaryWriter()
-num_epochs = 5
-outputs = []
-for epoch in range(num_epochs):
-    first = True
-    for (img_original, _) in data_loader:
-        img = img_original.reshape(-1, 28*28)
-        decoded_image, mean, log_var = model(img)
-        loss = calculate_loss(img, decoded_image, mean, log_var)
+    model = vae.VAELinear().to(device)
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+    writer = SummaryWriter()
+    num_epochs = 5
+    for epoch in range(num_epochs):
+        first = True
+        for (img_original, _) in data_loader:
+            img_original = img_original.to(device)
+            img = img_original.reshape(-1, 28*28)
+            decoded_image, mean, log_var = model(img)
+            loss = calculate_loss(img, decoded_image, mean, log_var)
 
-        if first:
-            writer.add_scalar("Loss/train", loss, epoch)
-            sample_generated_images = decoded_image.reshape(-1,1,28, 28)
-            grid = torchvision.utils.make_grid(img_original[0:5])
-            writer.add_image(f"{epoch} - Original", grid)
-            grid = torchvision.utils.make_grid(sample_generated_images[0:5])
-            writer.add_image(f"{epoch} - Generated", grid)
-            writer.flush()
-            first = False
+            if first:
+                writer.add_scalar("Loss/train", loss, epoch)
+                sample_generated_images = decoded_image.reshape(-1,1,28, 28)
+                grid = torchvision.utils.make_grid(img_original[0:5])
+                writer.add_image(f"{epoch} - Original", grid)
+                grid = torchvision.utils.make_grid(sample_generated_images[0:5])
+                writer.add_image(f"{epoch} - Generated", grid)
+                writer.flush()
+                first = False
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print(f"End of epoch {epoch}")
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print(f"End of epoch {epoch}")
 
-torch.save(model.state_dict(), 'models/vae.h5')
-writer.close()
+    torch.save(model.state_dict(), 'models/vae.h5')
+    writer.close()
+
+start = time.time()
+train()
+end = time.time()
+
+print(f"Required time {end-start}")
